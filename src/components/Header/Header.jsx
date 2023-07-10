a76683de17b3c84e9e9f194142f05db0871e29c8
Yakovleva Ksenia <bezdenejnyhk@yandex.ru>
2023-07-10 14:46:14 +0300

    Исправление ошибок

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import "./Header.css";
 import Navigation from "../Navigation/Navigation";
 import Logo from "../../images/logo.svg";
-import { Link } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 import Drawer from "../Drawer/Drawer";
 import useResize from "../../hooks/useResize";
 
 const Header = ({ isLoggedIn }) => {
   const size = useResize();
+  let location = useLocation();
   return (
-    <header className="header">
+    <header className={location.pathname === "/" ? "header_color" : "header"}>
       <Link className="header__route" to="/">
         <img className="header__logo" src={Logo} alt="Логотип" />
       </Link>
